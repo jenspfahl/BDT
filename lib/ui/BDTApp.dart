aa98da5853300606d8e215ecfe200717464879e4
Jens Pfahl <jens.pfahl@mailbox.org>
2026-09-19 21:27:49 +0200

    Fix app colors

--- lib/ui/BDTApp.dart
+++ lib/ui/BDTApp.dart
@@ -22,79 +22,80 @@ class BDTApp extends StatelessWidget {
           stream: prefsUpdatedNotifier.stream,
           builder: (context, snapshot) {
 
-            return MaterialApp(
-              title: APP_NAME_SHORT,
-              localizationsDelegates: [
-                AppLocalizations.delegate, // use  flutter gen-l10n if you add new languages
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-              ],
-              supportedLocales: _ensureEnglishFirst(AppLocalizations.supportedLocales),
-              theme: ThemeData(
-                  useMaterial3: false,
-                  brightness: Brightness.light,
-                  primaryColor: ColorService()
-                      .getCurrentScheme()
-                      .primary,
-                  primarySwatch: ColorService()
-                      .getCurrentScheme()
-                      .button,
-                  scaffoldBackgroundColor: ColorService()
-                      .getCurrentScheme()
-                      .background,
+            return DynamicColorBuilder(
+              builder: (lightDynamic, darkDynamic) {
+                debugPrint('lightDynamic1: ${lightDynamic?.primary}');
+                debugPrint('darkDynamic1: ${darkDynamic?.primary}');
 
-                  appBarTheme: AppBarTheme(
-                    backgroundColor: ColorService()
-                        .getCurrentScheme()
-                        .background,
-                    foregroundColor: ColorService()
-                        .getCurrentScheme()
-                        .foreground,
-                  )
-                // accentColor: Colors.green,
-              ),
-              darkTheme: ThemeData(
-                  useMaterial3: false,
-                  brightness: Brightness.dark,
-                  primaryColor: ColorService()
-                      .getCurrentScheme()
-                      .primary,
-                  primarySwatch: ColorService()
-                      .getCurrentScheme()
-                      .button,
-                  scaffoldBackgroundColor: ColorService()
-                      .getCurrentScheme()
-                      .background,
+                ColorService().isDynamicColorsSupported = darkDynamic != null && lightDynamic != null;
 
-                  appBarTheme: AppBarTheme(
-                    backgroundColor: ColorService()
-                        .getCurrentScheme()
-                        .background,
-                    foregroundColor: ColorService()
-                        .getCurrentScheme()
-                        .foreground,
-                  )
-              ),
-              themeMode: PreferenceService().darkTheme
-                  ? ThemeMode.dark
-                  : ThemeMode.light,
-
-              home: DynamicColorBuilder(
-                  builder: (lightDynamic, darkDynamic) {
-                    debugPrint('lightDynamic1: ${lightDynamic?.primary}');
-                    debugPrint('darkDynamic1: ${darkDynamic?.primary}');
-
-                    ColorService().isDynamicColorsSupported = darkDynamic != null && lightDynamic != null;
-
-                    if (ColorService().isDynamicColorsSupported && PreferenceService().useSystemColors) {
-                      ColorService().setDynamicColors(
-                          darkDynamic!.primary,
-                          lightDynamic!.primary
-                      );
-                    }
-                    return BDTScaffold();
+                if (ColorService().isDynamicColorsSupported && PreferenceService().useSystemColors) {
+                  ColorService().setDynamicColors(
+                      darkDynamic!.primary,
+                      lightDynamic!.primary
+                  );
                 }
-              ),
+
+                return MaterialApp(
+                  title: APP_NAME_SHORT,
+                  localizationsDelegates: [
+                    AppLocalizations.delegate, // use  flutter gen-l10n if you add new languages
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                  ],
+                  supportedLocales: _ensureEnglishFirst(AppLocalizations.supportedLocales),
+                  theme: ThemeData(
+                      useMaterial3: false,
+                      brightness: Brightness.light,
+                      primaryColor: ColorService()
+                          .getCurrentScheme()
+                          .primary,
+                      primarySwatch: ColorService()
+                          .getCurrentScheme()
+                          .button,
+                      scaffoldBackgroundColor: ColorService()
+                          .getCurrentScheme()
+                          .background,
+
+                      appBarTheme: AppBarTheme(
+                        backgroundColor: ColorService()
+                            .getCurrentScheme()
+                            .background,
+                        foregroundColor: ColorService()
+                            .getCurrentScheme()
+                            .foreground,
+                      )
+                    // accentColor: Colors.green,
+                  ),
+                  darkTheme: ThemeData(
+                      useMaterial3: false,
+                      brightness: Brightness.dark,
+                      primaryColor: ColorService()
+                          .getCurrentScheme()
+                          .primary,
+                      primarySwatch: ColorService()
+                          .getCurrentScheme()
+                          .button,
+                      scaffoldBackgroundColor: ColorService()
+                          .getCurrentScheme()
+                          .background,
+
+                      appBarTheme: AppBarTheme(
+                        backgroundColor: ColorService()
+                            .getCurrentScheme()
+                            .background,
+                        foregroundColor: ColorService()
+                            .getCurrentScheme()
+                            .foreground,
+                      )
+                  ),
+                  themeMode: PreferenceService().darkTheme
+                      ? ThemeMode.dark
+                      : ThemeMode.light,
+
+                  home: BDTScaffold(),
+                );
+              }
             );
 
           }
